b3efe609d7a24ca8f7029e211bd76d5c5e258d4f
husna <husnazghazali@gmail.com>
2024-03-01 10:36:06 +0100

    loading and showing images

--- src/FoodShow.js
+++ src/FoodShow.js
@@ -1,5 +1,25 @@
+import veggie from './imgfood/veggie.jpg';
+import cake from './imgfood/cake.jpg';
+import fruit from './imgfood/fruit.jpg';
+import noodle from './imgfood/noodle.jpg';
+import spice from './imgfood/spice.jpg';
+import sushi from './imgfood/sushi.jpg';
+import pasta from './imgfood/pasta.jpg';
+
+
+const imgfoodMap = {
+    veggie,
+    cake,
+    fruit,
+    noodle,
+    spice,
+    sushi,
+    pasta,
+};
+
 function FoodShow({ type }) {
-    return <div>{type}</div>;
+    return <div>
+        <img alt="food" src={imgfoodMap[type]} /></div>;
 }
 
 export default FoodShow;
